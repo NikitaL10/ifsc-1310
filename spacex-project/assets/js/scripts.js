f25eaac1bfe2bc208d883c8b4a34b445aa5ed307
NikitaL10 <143356719+NikitaL10@users.noreply.github.com>
2023-11-03 00:46:13 +0000

    Scrolling Update

--- spacex-project/assets/js/scripts.js
+++ spacex-project/assets/js/scripts.js
@@ -1,10 +1,30 @@
 var prevScrollpos = window.scrollY;
+var navbar = document.getElementById("navbar");
+var scrollHeightThreshold = 440; // Set the desired scroll height (in pixels) before the navbar disappears
+
 window.onscroll = function() {
   var currentScrollPos = window.scrollY;
+  
   if (prevScrollpos > currentScrollPos) {
+    // When the user scrolls up, the navbar appears
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-60px";
+    if (currentScrollPos > scrollHeightThreshold) {
+      // When the user scrolls down after 440 pixels, the navbar disappears
+      document.getElementById("navbar").style.top = "-60px";
+    }
   }
+  
   prevScrollpos = currentScrollPos;
+}
+
+
+
+function toggleMenu() {
+  var navigation = document.querySelector(".navigation");
+  if (navigation.style.display === "block") {
+    navigation.style.display = "none";
+  } else {
+    navigation.style.display = "block";
+  }
 }
