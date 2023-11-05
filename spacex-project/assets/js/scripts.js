ffddcd089886965a886b50436523e233f8328b70
NikitaL10 <143356719+NikitaL10@users.noreply.github.com>
2023-11-05 02:32:54 +0000

    scroll down

--- spacex-project/assets/js/scripts.js
+++ spacex-project/assets/js/scripts.js
@@ -29,10 +29,3 @@ function toggleMenu() {
   }
 }
 
-
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
