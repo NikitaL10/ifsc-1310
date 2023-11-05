3048b69f35171202d6fb525f6728d144a4fd9c0d
NikitaL10 <143356719+NikitaL10@users.noreply.github.com>
2023-11-05 03:16:39 +0000

    Small monitors update

--- spacex-project/assets/js/scripts.js
+++ spacex-project/assets/js/scripts.js
@@ -1,16 +1,14 @@
 var prevScrollpos = window.scrollY;
 var navbar = document.getElementById("navbar");
-var scrollHeightThreshold = 440; // Set the desired scroll height (in pixels) before the navbar disappears
+var scrollHeightThreshold = 440; 
 
 window.onscroll = function() {
   var currentScrollPos = window.scrollY;
   
   if (prevScrollpos > currentScrollPos) {
-    // When the user scrolls up, the navbar appears
     document.getElementById("navbar").style.top = "0";
   } else {
     if (currentScrollPos > scrollHeightThreshold) {
-      // When the user scrolls down after 440 pixels, the navbar disappears
       document.getElementById("navbar").style.top = "-60px";
     }
   }
